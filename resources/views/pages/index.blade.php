c1a17a815fa73f1e64a55b13376fcaa2bc360c9d
NickSpuy <0957166@hr.nl>
2018-10-15 01:28:07 +0200

    changed homepage

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,6 +1,8 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>{{$title}}</h1>
-    <p>This is my application</p>
+    <div class="jumbotron text-center">
+        <h1>Welcome to Laravel!</h1>
+        <p>This is my application</p>
+        <p><a class="btn btn-primary btn-lg" href="/login" role="button">Login</a> <a class="btn btn-succes btn-lg" href="/register" role="button">Register</a></p>
 @endsection
