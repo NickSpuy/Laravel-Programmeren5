742c113d3b3bb79c16a5e20144b3da8efb27e124
NickSpuy <0957166@hr.nl>
2018-10-30 18:55:18 +0100

    Added CRUD

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -19,7 +19,7 @@ class PostsController extends Controller
         // $posts = Post::orderBy('title', 'desc')->take(1)->get();
         // $posts = Post::orderBy('title', 'desc')->get();
 
-        $posts = Post::orderBy('title', 'desc')->paginate(10);
+        $posts = Post::orderBy('created_at', 'desc')->paginate(10);
         return view('posts.index')->with('posts', $posts);
     }
 
@@ -30,7 +30,7 @@ class PostsController extends Controller
      */
     public function create()
     {
-        //
+        return view('posts.create');
     }
 
     /**
@@ -41,7 +41,18 @@ class PostsController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $this->validate($request, [
+            'title' => 'required',
+            'body' => 'required' 
+        ]);
+
+        // Create Post
+        $post = new Post;
+        $post->title = $request->input('title');
+        $post->body = $request->input('body');
+        $post->save();
+
+        return redirect('/posts')->with('succes', 'Post Created');
     }
 
     /**
@@ -64,7 +75,8 @@ class PostsController extends Controller
      */
     public function edit($id)
     {
-        //
+        $post = Post::find($id);
+        return view('posts.edit')->with('post', $post);
     }
 
     /**
@@ -76,7 +88,18 @@ class PostsController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $this->validate($request, [
+            'title' => 'required',
+            'body' => 'required' 
+        ]);
+
+        // Create Post
+        $post = Post::find($id);
+        $post->title = $request->input('title');
+        $post->body = $request->input('body');
+        $post->save();
+
+        return redirect('/posts')->with('succes', 'Post Updated');
     }
 
     /**
@@ -87,6 +110,9 @@ class PostsController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $post = Post::find($id);
+        $post->delete();
+
+        return redirect('/posts')->with('succes', 'Post Removed');
     }
 }
