41a2d911e85b91cc910fbadb3e1214c481f1e978
NickSpuy <0957166@hr.nl>
2018-11-04 18:38:04 +0100

    bugfix

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -4,5 +4,4 @@
     <div class="jumbotron text-center">
         <h1>Welcome to Laravel!</h1>
         <p>This is my application</p>
-        <p><a class="btn btn-primary btn-lg" href="/login" role="button">Login</a> <a class="btn btn-succes btn-lg" href="/register" role="button">Register</a></p>
 @endsection
